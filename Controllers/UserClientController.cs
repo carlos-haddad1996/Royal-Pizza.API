976eebfa9c6b43589ce99b7f8e90e8885c43bef6
Carlos Haddad <carlos.haddad@partnerhero.com>
2020-03-27 21:03:57 -0600

    Updated get All Users method

--- Controllers/UserClientController.cs
+++ Controllers/UserClientController.cs
@@ -11,6 +11,50 @@ namespace Royal_Pizza.API.Controllers
     [ApiController]
     public class CrunchyController : ControllerBase
     {
+
+        [Route("users")]
+        [HttpGet]
+        public ActionResult<List<User>> Get()
+        {
+            Database db = new Database();
+            db.dbConnection.Open();
+            var command = db.dbConnection.CreateCommand();
+            command.CommandText = "SELECT * FROM Users";
+            string tmp = "[";
+            var read = command.ExecuteReader();
+            var first = true;
+
+            while (read.Read())
+            {
+                for (int i = 0; i <= read.VisibleFieldCount - 1; i++)
+                {
+                    var r = read.GetValue(i);
+                    var val = "";
+                    if (i == 0 || i == 3)
+                        val = r.ToString();
+                    else
+                        val = "\"" + r.ToString() + "\"";
+
+                    if (first && i == 0)
+                    {
+                        tmp += @"{""User_ID"": " + val + ",";
+                        first = false;
+                    }
+                    else if (i == 0 && !first)
+                        tmp += @",{""User_ID"": " + val + ",";
+                    else if (i == 1)
+                        tmp += @"""Username"": " + val + ",";
+                    else if (i == 2)
+                        tmp += @"""Password"": " + val + ",";
+                    else if (i == 3)
+                        tmp += @"""Client_ID"": " + val + ",";
+                }
+                tmp += "}";
+            }
+            tmp += "]";
+            return JsonConvert.DeserializeObject<List<User>>(tmp);
+        }
+
         [Route("users/{user}")]
         [HttpGet]
         public ActionResult<User> Get(string user)
